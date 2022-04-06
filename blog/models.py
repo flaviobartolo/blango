6f5ee417b9254dab620f6e682161c68313b57d99
Flavio Bartolo <flavio.bartolo2@gmail.com>
2022-04-06 14:29:50 +0000

    Finish custom template tags

--- blog/models.py
+++ blog/models.py
@@ -22,6 +22,9 @@ class Comment(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   modified_at = models.DateTimeField(auto_now=True)
 
+  def __str__(self):
+    return self.creator.username
+
 
 class Post(models.Model):
   author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
